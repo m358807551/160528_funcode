519e982cdb0cd6427abf92ac700881684391f453
m358807551 <m358807551@163.com>
2016-05-31 23:14:26 +0800

    [features] add something in app_django models

--- funcode/app_rzrk/views.py
+++ funcode/app_rzrk/views.py
@@ -1,3 +1,22 @@
+import random
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from app_rzrk.models import MRzrk
 
+def randquestionid():
+    return int(random.uniform(0, len(MRzrk.objects.all())))
+
+nowquestionid = randquestionid()
 # Create your views here.
+def index(request):
+    global nowquestionid
+    question = MRzrk.objects.all()[nowquestionid].question
+    return render_to_response('index.html', {'app':'rzrk', 'question': question})
+
+def judge(request):
+    global nowquestionid
+    answer = request.GET.get('answer', 'None')
+    if answer == MRzrk.objects.all()[nowquestionid].solution:
+       nowquestionid = randquestionid()
+    return index(request)
--- funcode/funcode/urls.py
+++ funcode/funcode/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from app_django import views as app_django_views
 from app_cplusplus import views as app_cplusplus_views
+from app_rzrk import views as app_rzrk_views
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,4 +14,6 @@ urlpatterns = patterns('',
     url(r'^django/judge/', app_django_views.judge),
     url(r'^cplusplus/index/', app_cplusplus_views.index),
     url(r'^cplusplus/judge/', app_cplusplus_views.judge),
+    url(r'^rzrk/index/', app_rzrk_views.index),
+    url(r'^rzrk/judge/', app_rzrk_views.judge),
 )
